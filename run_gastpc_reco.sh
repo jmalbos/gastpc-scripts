9d16336c7fe18e55e9afd9341086b4db6d33dde4
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2017-02-02 10:03:16 +0000

    New version of the script.

--- run_gastpc_reco.sh
+++ run_gastpc_reco.sh
@@ -1,100 +1,89 @@
 #!/usr/bin/env bash
 
 print_usage() {
-  echo "Usage: `basename ${0}` <mode> <lower_bound> <upper_bound>"
+  echo "Usage: `basename ${0}` <mode> <job_group>"
 }
 
-ceiling_divide() {
-  ceiling_result=$((($1+$2-1)/$2))
-  echo $ceiling_result
-}
+############################################################
 
-
-if [[ $# -le 2 ]]; then
+if [[ $# -le 1 ]]; then
   print_usage
   exit 1
 fi
 
-
-############################################################
-
 FLAVOUR=$1
-LOWER_BOUND=$2
-UPPER_BOUND=$3
-
-if [ "${FLAVOUR}" == "neutrino" ]; then
-  NU_EVENTS=125
-  GENIE_EVENTS=25474 # 125 times 200 plus 3 sigma
-  ROCK_EVENTS=666
-  ROCK_LISTFILE="rock_${FLAVOUR}.txt"
-elif [ "${FLAVOUR}" == "antineutrino" ]; then
-  NU_EVENTS=50
-  GENIE_EVENTS=10300 # 50 times 200 plus 3 sigma
-  ROCK_EVENTS=448
-  ROCK_LISTFILE="rock_${FLAVOUR}.txt"
+if ( [ "${FLAVOUR}" == "neutrino" ] || [ "${FLAVOUR}" == "antineutrino" ] )
+then
+  echo "-- Beam mode: [ ${FLAVOUR} ]"
 else
   print_usage
   echo '       <mode>: neutrino | antineutrino'
   exit 1
 fi
 
-echo "-- Flavour mode: ${FLAVOUR}"
-echo "-- Job number range: [ ${LOWER_BOUND}, ${UPPER_BOUND} ]"
+JOB_GROUP=$2
+printf -v JOB_GROUP '%02.0f' ${JOB_GROUP}
+echo "-- Job group: [ ${JOB_GROUP} ]"
 
 ############################################################
 
 USRDIR='/dune/app/users/jmalbos/ndtf/4RT'
-OUTDIR='/pnfs/dune/tape_backed/dunepro/mc/neardet/gartpc/ndtf-4rt'
-FLUXAPP='copy_dune_ndtf_flux'
+DATADIR='/pnfs/dune/tape_backed/dunepro/mc/neardet/gartpc/ndtf-4rt'
+#SCRIPT=${USRDIR}/${FLAVOUR}.${JOB_GROUP}.sh
+SCRIPT=$PWD/file.sh
 
-############################################################
+######################################################################
 
-setup jobsub_client
+echo '#!/usr/bin/env bash'                                >  ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
+echo "USRDIR=${USRDIR}"                                   >> ${SCRIPT}
+echo "DATADIR=${DATADIR}"                                 >> ${SCRIPT}
+echo "FLAVOUR=${FLAVOUR}"                                 >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
+echo "JOB_GROUP=${JOB_GROUP}"                             >> ${SCRIPT}
+echo "JOB_NUMBER=$((JOB_GROUP * 1000 + PROCESS))"         >> ${SCRIPT}
+echo 'printf -v JOB_NUMBER '\'%05.0f\'' ${JOB_NUMBER}'    >> ${SCRIPT}
+echo 'echo "-- Job number: [ ${JOB_NUMBER} ]"'            >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
+echo 'INDIR=${DATADIR}/sim/${FLAVOUR}/${JOB_GROUP}'       >> ${SCRIPT}
+echo 'OUTDIR=${DATADIR}/dst/${FLAVOUR}/${JOB_GROUP}'      >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
+echo 'INFILE=${FLAVOUR}.${JOB_NUMBER}.g4sim.root'         >> ${SCRIPT}
+echo 'OUTFILE=${FLAVOUR}.${JOB_NUMBER}.dst.root'          >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
 
-for i in `seq ${LOWER_BOUND} ${UPPER_BOUND}`; do
+### Initialize ups and setup required products #######################
+echo 'source /grid/fermiapp/products/dune/setup_dune.sh'  >> ${SCRIPT}
+echo 'setup gastpc v2_4 -q e10:prof'                      >> ${SCRIPT}
+#echo 'setup genie v2_10_10 -q e10:prof:r6'                >> ${SCRIPT}
+echo 'setup genie_xsec v2_10_6 -q defaultplusccmec'       >> ${SCRIPT}
+echo 'setup genie_phyopt v2_10_6 -q dkcharmtau'           >> ${SCRIPT}
+echo 'setup ifdhc'                                        >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
 
-  printf -v JOB_NUMBER '%05.0f' ${i}
-  JOB_GROUP=${JOB_NUMBER:0:2}
+### Copy input file ##################################################
+echo 'ifdh cp \'                                          >> ${SCRIPT}
+echo ' ${INDIR}/${INFILE} \'                              >> ${SCRIPT}
+echo ' ${INFILE}'                                         >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
 
-  #echo "-- Job number: ${JOB_NUMBER}"
-  #echo "-- Job group: ${JOB_GROUP}"
+### Run pseudo-reconstruction ########################################
+echo 'GasTPCReco \'                                       >> ${SCRIPT}
+echo ' -r ${JOB_NUMBER} \'                                >> ${SCRIPT}
+echo ' -m '${FLAVOUR}' \'                                 >> ${SCRIPT}
+echo ' -i ${INFILE} \'                                    >> ${SCRIPT}
+echo ' -o ${OUTFILE}'                                     >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
 
-  # We'll write a script for each job in the range chosen by the user
-  SCRIPT=${USRDIR}/${FLAVOUR}.${JOB_NUMBER}.sh
+### Copy files to dCache #############################################
+echo 'ifdh cp ${OUTFILE} \'                               >> ${SCRIPT}
+echo ' ${OUTDIR}/${OUTFILE}'                              >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
+echo 'rm ${INFILE}'                                       >> ${SCRIPT}
+echo 'rm ${OUTFILE}'                                      >> ${SCRIPT}
+echo ''                                                   >> ${SCRIPT}
 
-  ### Initialize ups and setup required products #######################
-  echo '#!/usr/bin/env bash'                                >  ${SCRIPT}
-  echo ''                                                   >> ${SCRIPT}
-  echo 'source /grid/fermiapp/products/dune/setup_dune.sh'  >> ${SCRIPT}
-  echo 'setup gastpc v2_4 -q e10:prof'                      >> ${SCRIPT}
-# echo 'setup genie v2_10_10 -q e10:prof:r6'                >> ${SCRIPT}
-  echo 'setup genie_xsec v2_10_6 -q defaultplusccmec'       >> ${SCRIPT}
-  echo 'setup genie_phyopt v2_10_6 -q dkcharmtau'           >> ${SCRIPT}
-  echo 'setup ifdhc'                                        >> ${SCRIPT}
-  echo ''                                                   >> ${SCRIPT}
-
-  ### Copy simulation files ############################################
-  echo 'ifdh cp \'                                                                      >> ${SCRIPT}
-  echo ' '${OUTDIR}/sim/${FLAVOUR}/${JOB_GROUP}/${FLAVOUR}.${JOB_NUMBER}.g4sim.root' \' >> ${SCRIPT}
-  echo ' input.g4sim.root'                                                              >> ${SCRIPT}
-  echo ''                                                                               >> ${SCRIPT}
-
-  ### Run pseudo-reconstruction ########################################
-  echo 'GasTPCReco \'                                       >> ${SCRIPT}
-  echo ' -r '${JOB_NUMBER}' \'                              >> ${SCRIPT}
-  echo ' -m '${FLAVOUR}' \'                                 >> ${SCRIPT}
-  echo ' -i input.g4sim.root \'                             >> ${SCRIPT}
-  echo ' -o '${FLAVOUR}.${JOB_NUMBER}.dst.root              >> ${SCRIPT}
-
-  ### Copy files to dCache #############################################
-  echo 'ifdh cp '${FLAVOUR}.${JOB_NUMBER}.dst.root' \'                         >> ${SCRIPT}
-  echo ${OUTDIR}/dst/${FLAVOUR}/${JOB_GROUP}/${FLAVOUR}.${JOB_NUMBER}.dst.root >> ${SCRIPT}
-  echo ''                                                                      >> ${SCRIPT}
-  echo 'rm input.g4sim.root'                                                   >> ${SCRIPT}
-  echo 'rm '${FLAVOUR}.${JOB_NUMBER}.dst.root                                  >> ${SCRIPT}
-
-  jobsub_submit \
-   --group dune --role=Analysis -N 1 --OS=SL6 --expected-lifetime=8h \
-   file://${SCRIPT}
-
-done
+#setup jobsub_client
+#jobsub_submit \
+#  --group dune --role=Analysis -N 1000 --OS=SL6 --expected-lifetime=8h \
+#  file://${SCRIPT}
