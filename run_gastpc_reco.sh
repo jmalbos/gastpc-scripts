2b9ef7f152bb2c33e3fcf0f9c64950c1e655aad9
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2017-03-01 16:11:41 +0000

    Updated for 5th run-through.

--- run_gastpc_reco.sh
+++ run_gastpc_reco.sh
@@ -4,14 +4,15 @@ print_usage() {
   echo "Usage: `basename ${0}` <mode> <job_group>"
 }
 
-############################################################
-
 if [[ $# -le 1 ]]; then
   print_usage
   exit 1
 fi
 
+############################################################
+
 FLAVOUR=$1
+
 if ( [ "${FLAVOUR}" == "neutrino" ] || [ "${FLAVOUR}" == "antineutrino" ] )
 then
   echo "-- Beam mode: [ ${FLAVOUR} ]"
@@ -21,32 +22,32 @@ else
   exit 1
 fi
 
+############################################################
+
 JOB_GROUP=$2
-printf -v JOB_GROUP '%02.0f' ${JOB_GROUP}
+printf -v JOB_GROUP '%02.0f' ${JOB_GROUP} # change to two-digit format
 echo "-- Job group: [ ${JOB_GROUP} ]"
 
 ############################################################
 
-USRDIR='/dune/app/users/jmalbos/ndtf/4RT'
-DATADIR='/pnfs/dune/tape_backed/dunepro/mc/neardet/gartpc/ndtf-4rt'
+USER_DIR='/dune/app/users/jmalbos/ndtf/4RT'
+INPUT_DIR='/pnfs/dune/tape_backed/dunepro/mc/neardet/gartpc/ndtf-4rt'
+OUTPUT_DIR='/pnfs/dune/persistent/TaskForce_AnaTree/hptcpnd/5thrun/dst'
 SCRIPT=${USRDIR}/${FLAVOUR}.${JOB_GROUP}.sh
 
 ######################################################################
 
 echo '#!/usr/bin/env bash'                                >  ${SCRIPT}
 echo ''                                                   >> ${SCRIPT}
-echo "USRDIR=${USRDIR}"                                   >> ${SCRIPT}
-echo "DATADIR=${DATADIR}"                                 >> ${SCRIPT}
+echo "USER_DIR=${USER_DIR}"                               >> ${SCRIPT}
+echo "INPUT_DIR=${INPUT_DIR}"                             >> ${SCRIPT}
+echo "OUTPUT_DIR=${OUTPUT_DIR}"                           >> ${SCRIPT}
 echo "FLAVOUR=${FLAVOUR}"                                 >> ${SCRIPT}
 echo ''                                                   >> ${SCRIPT}
 echo "JOB_GROUP=${JOB_GROUP}"                             >> ${SCRIPT}
-echo 'echo ${PROCESS}' >> ${SCRIPT}
 echo 'JOB_NUMBER=$((10#$JOB_GROUP * 1000 + PROCESS))'     >> ${SCRIPT}
 echo 'printf -v JOB_NUMBER '\'%05.0f\'' ${JOB_NUMBER}'    >> ${SCRIPT}
 echo 'echo "-- Job number: [ ${JOB_NUMBER} ]"'            >> ${SCRIPT}
-echo ''                                                   >> ${SCRIPT}
-echo 'INDIR=${DATADIR}/sim/${FLAVOUR}/${JOB_GROUP}'       >> ${SCRIPT}
-echo 'OUTDIR=${DATADIR}/dst/${FLAVOUR}/${JOB_GROUP}'      >> ${SCRIPT}
 echo ''                                                   >> ${SCRIPT}
 echo 'INFILE=${FLAVOUR}.${JOB_NUMBER}.g4sim.root'         >> ${SCRIPT}
 echo 'OUTFILE=${FLAVOUR}.${JOB_NUMBER}.dst.root'          >> ${SCRIPT}
@@ -54,8 +55,7 @@ echo ''                                                   >> ${SCRIPT}
 
 ### Initialize ups and setup required products #######################
 echo 'source /grid/fermiapp/products/dune/setup_dune.sh'  >> ${SCRIPT}
-echo 'setup gastpc v2_5_2 -q e10:prof'                    >> ${SCRIPT}
-#echo 'setup genie v2_10_10 -q e10:prof:r6'                >> ${SCRIPT}
+echo 'setup gastpc v3_2 -q e10:prof'                      >> ${SCRIPT}
 echo 'setup genie_xsec v2_10_6 -q defaultplusccmec'       >> ${SCRIPT}
 echo 'setup genie_phyopt v2_10_6 -q dkcharmtau'           >> ${SCRIPT}
 echo 'setup ifdhc'                                        >> ${SCRIPT}
@@ -63,12 +63,12 @@ echo ''                                                   >> ${SCRIPT}
 
 ### Copy input file ##################################################
 echo 'ifdh cp \'                                          >> ${SCRIPT}
-echo ' ${INDIR}/${INFILE} \'                              >> ${SCRIPT}
+echo ' ${INPUT_DIR}/${INFILE} \'                          >> ${SCRIPT}
 echo ' ${INFILE}'                                         >> ${SCRIPT}
 echo ''                                                   >> ${SCRIPT}
 
 ### Run pseudo-reconstruction ########################################
-echo 'GasTPCReco \'                                       >> ${SCRIPT}
+echo 'GasTPCPseudoReco \'                                 >> ${SCRIPT}
 echo ' -r ${JOB_NUMBER} \'                                >> ${SCRIPT}
 echo ' -m '${FLAVOUR}' \'                                 >> ${SCRIPT}
 echo ' -i ${INFILE} \'                                    >> ${SCRIPT}
@@ -77,7 +77,7 @@ echo ''                                                   >> ${SCRIPT}
 
 ### Copy files to dCache #############################################
 echo 'ifdh cp ${OUTFILE} \'                               >> ${SCRIPT}
-echo ' ${OUTDIR}/${OUTFILE}'                              >> ${SCRIPT}
+echo ' ${OUTPUT_DIR}/${FLAVOUR}/${JOB_GROUP}/${OUTFILE}'  >> ${SCRIPT}
 echo ''                                                   >> ${SCRIPT}
 echo 'rm ${INFILE}'                                       >> ${SCRIPT}
 echo 'rm ${OUTFILE}'                                      >> ${SCRIPT}
